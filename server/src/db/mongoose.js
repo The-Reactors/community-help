8a2c93d021ad6551737bd54b57e394aaf06d7021
prabs3257 <prabhavracer44@gmail.com>
2021-09-02 01:57:03 +0530

    Changed the Connection String

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://shivam:nayak@cluster0.fjg0d.mongodb.net/nayak", (err) => {
+mongoose.connect("mongodb://shivam:nayak@cluster0-shard-00-00.fjg0d.mongodb.net:27017,cluster0-shard-00-01.fjg0d.mongodb.net:27017,cluster0-shard-00-02.fjg0d.mongodb.net:27017/nayak?ssl=true&replicaSet=atlas-qnsl1m-shard-0&authSource=admin&retryWrites=true&w=majority", (err) => {
   
     if (err) throw err;
     console.log("connected to MongoDB");
