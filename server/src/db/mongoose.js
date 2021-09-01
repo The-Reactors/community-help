4e9bd64766a4f9a9a36a8f3f878b2cd8e3f40f01
prabs3257 <prabhavracer44@gmail.com>
2021-09-02 00:39:16 +0530

    authenticatition added

---
+++ server/src/db/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://127.0.0.1:27017/community-help-api", (err) => {
+  
+    if (err) throw err;
+    console.log("connected to MongoDB");
+});
---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const userRouter = require('./routers/user')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+
+
+app.listen(port, ()=>{
+    console.log('Server is running on ' + port)
+})
