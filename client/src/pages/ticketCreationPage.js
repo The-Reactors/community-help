1374368e5d36dc5388271e62db514cff6a0a2b06
utkrisht30 <usingh_be20@thapar.edu>
2021-09-03 04:52:56 +0530

    ticketCreated

--- client/src/pages/ticketCreationPage.js
+++ client/src/pages/ticketCreationPage.js
@@ -1,74 +1,97 @@
-import "../assets/css/style.css"
-import "../assets/css/bootstrap.min.css"
+import React, { useState } from 'react'
 
 const TicketCreationPage = () => {
-    return ( 
- <div className="container">
-    <div className="row">
-      <div className="col-xl-8 mx-auto">
-        {/* <!--Ticket Input Box--> */}
-        <h2 className="text-center mt-4">Raise a ticket</h2>
-        
-        <div id="ticketBox">
-          <div id="ticketBoxCenter">
-            <label className="mt-3">Issue</label>
-            <input id="mainIssue" className="form-control" />
-            <label className="mt-3">Severity</label>
-            <select id="severity" className="form-control">
-                <option>Low</option>
-                <option>Normal</option>
-                <option>High</option>
-                <option>Urgent</option>
-              </select>
-            <label className="mt-3">Issue Category</label>
-            <select id="issue Category" className="form-control">
-                <option>land issue
-  </option>
-                <option>water issue</option>
-                <option>public health</option>
-                <option>sanitation</option>
-                <option>Pollution</option>
-                <option>healthcare issue</option>
-                <option>Electricity</option>
-                <option>Road Blockage</option>
-                <option>waste management</option>
-                </select>
-            <label className="mt-3">Phone Number</label>
-            <input id="Phone number" className="form-control" type="number" />
-            <label className="mt-3">Details</label>
-            <textarea id="details" className="form-control" rows="2"></textarea>
-            <div className="text-center">
-              <p id="errMsg" className="text-center mt-2"></p>
-              <button id="submitTicket" className="btn btn-secondary text-center mt-2">Submit</button>
-            </div>
-          </div>
-          <p id="confirmation" className="text-center"></p>
-          <div className="text-center">
-            <button id="goBack" className="btn btn-secondary mt-3">Go Back</button>
-          </div>
-        </div>
-  
-        {/* <!--Ticket Status--> */}
-        <div id="ticketBoxTwo" className="mt-4">
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <button id="searchTicketButton" className="btn btn-secondary" type="button">Search</button>
-            </div>
-            <input id="searchTicketInput" type="number" className="form-control" placeholder="Ticket Number" aria-label="Example text with button addon"/>
-          </div>
-          <p className="ticket text-center"></p>
-          <p id="ticketStatus" className="text-center"></p>
-          <div className="text-center">
-            <button id="closeTicket" className="btn btn-primary mt-3" type="submit">Close</button>
-            <button id="deleteTicket" className="btn btn-danger mt-3" type="submit">Delete</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  
-</div> 
+    const [userEnteredData, setuserEnteredData] = useState({
+        title : "",
+        description : "",
+        priority: "",
+        status : "",
+        category : "",
+        kind : "",
+        image : ""
+    })
+    const [imageState, setimageState] = useState()
+    const handleInput = (event) =>
+    {
+        const name = event.target.name;
+        const value = event.target.value;
 
-  );
+        setuserEnteredData({...userEnteredData, [name]:value })
+
+    }
+
+    const fileHandler = (event) =>
+    {
+      console.log(event.target.files)
+      const file = event.target.files;
+      setimageState(file)
+    }
+
+    const fileSubmitHandler = (e) =>
+    {
+      e.preventDefault();
+      const data = new FormData()
+      for(var x = 0; x<imageState.length; x++) {
+        data.append('file', imageState[x])
+        console.log("h")
+        
+   }
+      console.log(data)
+      console.log(imageState) 
+      
+    }
+
+
+    return (
+        <div>
+           <form action="" encType = "multipart/form-data">
+               <label htmlFor="title">Title</label>
+               <input
+                   type = "text"
+                   name = "title"
+                   value = {userEnteredData.title}
+                   onChange = {handleInput}
+               />
+               <label htmlFor="description">Description</label>
+                <input 
+                type="text" 
+                autoComplete = "off"
+                value = {userEnteredData.description}
+                onChange = {handleInput}
+                name = "description"
+                />
+                <label htmlFor="priority">Select your Ticket Priority
+               <select onChange = {handleInput}>
+                <option value="emergency" name = " emergency">emergency</option>
+                <option value="urgent" name = " urgent ">urgent</option>
+                <option value="not urgent" name = " not urgent ">not urgent</option>
+                <option value="baad mein karwalenge" name = " baad mein karwalenge">baad mein karwalenge</option>
+                </select>
+                </label>
+                <label htmlFor="category">Select your category:
+               <select onChange = {handleInput}>
+               <option value = " land issue " name = " land issue ">land issue</option>
+                <option value = " water issue " name = " water issue">water issue</option>
+                <option value = " public health " name = " public health ">public health</option>
+                <option value = " sanitation " name = " sanitation ">sanitation</option>
+                <option value = " pollution " name = " pollution ">Pollution</option>
+                <option value = " healthcare issue " name = " healthcare issue ">healthcare issue</option>
+                <option value = " electricity " name = " electricity ">Electricity</option>
+                <option value = " road blockage " name = " road blockage ">Road Blockage</option>
+                <option value = " waste management " name = " waste management ">waste management</option>
+                </select>
+                </label>
+                <p>
+                <label htmlFor="uploads">
+                Choose the images you want to upload:
+                </label>
+                <input type="file" id="uploads" name="uploads" accept=".jpg, .jpeg, .png, .svg, .gif" multiple onChange = {fileHandler} />
+                <button onClick = {fileSubmitHandler}>Upload</button>
+                </p>
+                
+           </form> 
+        </div>
+    )
 }
- 
-export default TicketCreationPage ;
+
+export default TicketCreationPage
