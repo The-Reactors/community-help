791244318422866b4db7f2aa120844271174094a
shivamdargan <darganshivam2001@gmail.com>
2021-09-05 08:43:51 +0530

    Removed Show Map Button And Shifted To New Strategy

--- client/src/pages/ticketCreationPage.js
+++ client/src/pages/ticketCreationPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import URL from '../URL'
 import swal from 'sweetalert';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXN1ciIsImEiOiJja3Q2ZXhkYW4waHJwMm5xbHVrZnE2YjZ2In0.pQ-92peoEdKmKFJAi6DoSg';
 const TicketCreationPage = () => {
     const [userEnteredData, setuserEnteredData] = useState({
         title : "",
@@ -12,10 +14,11 @@ const TicketCreationPage = () => {
         kind : "issue",
     })
     const [imageState, setimageState] = useState()
-    const [latitude, setLatitude] = useState()
-    const [longitude, setLongitude] = useState(0)
-    const [submitFlag, setSubmitFlag] = useState(0)
-    const [ab, setAB] = useState({longitude, submitFlag});
+    const [latitude, setLatitude] = useState(30.3420432)
+    const [longitude, setLongitude] = useState(76.2895914)
+
+    let map = useRef(null);
+    let mapContainer = useRef(null);
 
     const handleInput = (event) =>
     {
@@ -42,6 +45,104 @@ const TicketCreationPage = () => {
       });
     };
 
+
+    const createMap = () =>
+    {
+        map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [longitude,latitude],
+        zoom: 12
+        });
+    }
+    // const firstMapUpdate= useRef(true)
+
+    // useEffect(() => {
+    //   if (map.current) return; // initialize map only once
+    //   if(firstMapUpdate.current){
+    //     firstMapUpdate.current =false 
+    //     return
+    //   }
+     
+    //   },[showMap]);
+
+      const problemCreator = () => {
+        let data = new FormData()
+        data.append('title',userEnteredData.title)
+        data.append('description',userEnteredData.description)
+        data.append('priority',userEnteredData.priority)
+        data.append('status',userEnteredData.status)
+        data.append('location',userEnteredData.location)
+        data.append('latitude',latitude)
+        data.append('longitude',longitude)
+        data.append('category',userEnteredData.category)
+        data.append('kind',userEnteredData.kind)
+        if(imageState !== undefined)
+        {
+            for(var x = 0; x<imageState.length; x++) {
+              data.append('problemImage', imageState[x])
+            }
+        }
+        console.log(data)
+        console.log(imageState) 
+        console.log(userEnteredData.category)
+        const requestOptions = {
+          method: 'POST',
+          body:data,  
+          credentials: "include"
+          };
+          fetch(`http://localhost:5000/problems`, requestOptions )
+          .then(async response => {
+              if(response.ok){
+                  console.log("Response Is Succesfully Done! ")
+                  swal({
+                    title: "Success!",
+                    text: "Ticket Raised Successfully",
+                    icon: "success",
+                  })
+                    
+                  createMap();
+               }
+               else if(response.status === 401){
+                swal({
+                  title: "Unauthorised!",
+                  text: "Please Login",
+                  icon: "error",
+                });
+               }
+              else{
+                  throw response.json();
+              }
+            })
+            .catch(async (error) => {
+              const errorMessage = await error;
+              console.log(errorMessage)
+              if( errorMessage.error !== undefined)
+              {
+                if(!(typeof errorMessage.error.code === 'string') && !(errorMessage.error.code instanceof String))  
+                {
+                  swal({
+                    title: "Error!",
+                    text: "Unknown Error Has Odfaccured !",
+                    icon: "error",
+                  });
+                }
+                else
+                {
+                  if((errorMessage.error.code.localeCompare("LIMIT_FILE_SIZE") === 0) ||(errorMessage.error.code.localeCompare("LIMIT_UNEXPECTED_FILE") === 0) )
+                  {
+                    swal({
+                      title: "Error!",
+                      text: "Maximumm Number Of 3 Images Can Be Uploaded",
+                      icon: "error",
+                    });
+                  }
+                }
+              }
+             
+            }) 
+    }
+
     const getLocation = async () =>{
       await fetch(`https://geocode.search.hereapi.com/v1/geocode?q=${userEnteredData.location}&apiKey=Bt-4s3hG9VlkF87RkELvh2Z1FVO3ih1i8GQ-keKlie8`, {credentials: "include"})
       .then(response => {
@@ -59,11 +160,7 @@ const TicketCreationPage = () => {
         console.log(error);
       });
     }
-    const locationHandler = async (e) =>
-    {
-        e.preventDefault();
-        await getLocation()
-      }
+      
     const fileHandler = (event) =>
     {
       console.log(event.target.files)
@@ -71,105 +168,20 @@ const TicketCreationPage = () => {
       setimageState(file)
     }
 
-    const firstUpdate = useRef(true)
-
-    useEffect(() => {
-      setAB(prev => {
-        console.log('prev AB', prev)
-        return (longitude !== prev.longitude && submitFlag !== prev.submitFlag) 
-          ? {longitude,submitFlag} 
-          : prev;  // do nothing
-      })
-    }, [longitude, submitFlag])
-
-
-    useEffect(()=>{
-      if(firstUpdate.current){
-        firstUpdate.current = false
-        return
-      }
-      let data = new FormData()
-      data.append('title',userEnteredData.title)
-      data.append('description',userEnteredData.description)
-      data.append('priority',userEnteredData.priority)
-      data.append('status',userEnteredData.status)
-      data.append('location',userEnteredData.location)
-      data.append('latitude',latitude)
-      data.append('longitude',longitude)
-      data.append('category',userEnteredData.category)
-      data.append('kind',userEnteredData.kind)
-      if(imageState !== undefined)
-      {
-          for(var x = 0; x<imageState.length; x++) {
-            data.append('problemImage', imageState[x])
-          }
-      }
-      console.log(data)
-      console.log(imageState) 
-      console.log(userEnteredData.category)
-      const requestOptions = {
-        method: 'POST',
-        body:data,  
-        credentials: "include"
-        };
-        fetch(`http://localhost:5000/problems`, requestOptions )
-        .then(async response => {
-            if(response.ok){
-                console.log("Response Is Succesfully Done! ")
-                swal({
-                  title: "Success!",
-                  text: "Ticket Raised Successfully",
-                  icon: "success",
-                });
-             }
-             else if(response.status === 401){
-              swal({
-                title: "Unauthorised!",
-                text: "Please Login",
-                icon: "error",
-              });
-             }
-            else{
-                throw response.json();
-            }
-          })
-          .catch(async (error) => {
-            const errorMessage = await error;
-            console.log(errorMessage)
-            if(!(typeof errorMessage.error.code === 'string') && !(errorMessage.error.code instanceof String))  
-            {
-              swal({
-                title: "Error!",
-                text: "Unknown Error Has Occured !",
-                icon: "error",
-              });
-            }
-            else
-            {
-              if((errorMessage.error.code.localeCompare("LIMIT_FILE_SIZE") === 0) ||(errorMessage.error.code.localeCompare("LIMIT_UNEXPECTED_FILE") === 0) )
-              {
-                swal({
-                  title: "Error!",
-                  text: "Maximumm Number Of 3 Images Can Be Uploaded",
-                  icon: "error",
-                });
-              }
-            }
-          })
-
-    }, [ab])
-    
-
     const fileSubmitHandler = async(e) =>
     {
       e.preventDefault();
 
       await getLocation()
-      setSubmitFlag(true)
-      
+      problemCreator();
     }
+    // const url="https://maps.google.com/maps?q=30.15787,84.20479&hl=es;z=14&amp;output=embed"
     return (
         <div>
+          {/* <iframe width="800" height="570" src = "https://maps.google.com/maps?q=30.15787,84.20479&hl=es;z=14&amp;output=embed" ></iframe> */}
+          <div>
+              <div ref={mapContainer} style={{height:"400px"}} />
+            </div>
            <form action="" encType = "multipart/form-data">
                <label htmlFor="title">Title</label>
                <input
@@ -194,7 +206,6 @@ const TicketCreationPage = () => {
                 value = {userEnteredData.location}
                 name = "location"
                 />
-                <button onClick = {locationHandler}>Show On Map</button>
                 <label htmlFor="priority">Select your Ticket Priority
                <select onChange={(e) => selectPriorityHandler(e)}>
                 <option value="emergency" name = "priority">emergency</option>
