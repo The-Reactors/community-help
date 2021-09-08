beb4eb85f3c683b2bb1ea7aa84068a18726cfaeb
prabs3257 <prabhavracer44@gmail.com>
2021-09-09 01:15:26 +0530

    completed support for multiple images in carousel

--- client/src/components/carousel.js
+++ client/src/components/carousel.js
@@ -2,71 +2,39 @@ import "../assets/css/carousel.css"
 import {useState} from "react"
 const Carousel = (props) => {
 
-    //console.log("hehehehe" + props.carouselId,props.images)
+    let imageListOne = (props.images.length >= 1) ? <li data-target={"#myCarousel"+props.carouselId} data-slide-to="0" className="active"></li> : null
+    let imageListTwo = (props.images.length >= 2) ? <li data-target={"#myCarousel"+props.carouselId} data-slide-to="1"></li> : null
+    let imageListThree = (props.images.length >= 3) ? <li data-target={"#myCarousel"+props.carouselId} data-slide-to="2"></li> : null
 
-    // const[indicatorButtons,setIndicatorButtons] = useState()
-    // const[imagesList,setImagesList] = useState()
-    
-    // if(props.images.length !== 0){
-       
-    //     let array = []
-    //     for(let i = 0; i < props.images.length; i++){
-    //         if(i === 0){
-    //             array.push(<li data-target={"#myCarousel"+props.carouselId} data-slide-to={i} className="active"></li>)
-    //             setIndicatorButtons(array)
-    //             //setIndicatorButtons(indicatorButtons +<li data-target={"#myCarousel"+props.carouselId} data-slide-to={i} className="active"></li>)
-    //         }else{
-    //             array.push(<li data-target={"#myCarousel"+props.carouselId} data-slide-to={i}></li>)
-    //             setIndicatorButtons(array)
-    //             //setIndicatorButtons(indicatorButtons +<li data-target={"#myCarousel"+props.carouselId} data-slide-to={i}></li>)
-    //         }
-            
-    //     }
-        
+    let imageDisplayOne = (imageListOne) ? <div className="item active">
+    <img className="size" src={`data:image/png;base64,${props.images[0]}`} alt="Los Angeles" style={{width:"100%"}}/>
+    </div> : null
 
-        
+    let imageDisplayTwo = (imageListTwo) ? <div className="item">
+    <img className="size" src={`data:image/png;base64,${props.images[1]}`} alt="Chicago" style={{width:"100%"}}/>
+    </div> : null
 
-        // for(let i = 0; i < props.images.length; i++){
-        //     if(i === 0){
-        //         imagesList = imagesList + 
-        //         <div className="item active"><img src={`data:image/png;base64,${props.images[i]}`} style={{width:"100%"}}/></div>
-        //     }else{
-        //         imagesList = imagesList + 
-        //         <div><img src={`data:image/png;base64,${props.images[i]}`} style={{width:"100%"}}/></div>
-        //     }
-            
-        // }
+    let imageDisplayThree = (imageListThree) ? <div className="item">
+    <img className="size" src={`data:image/png;base64,${props.images[2]}`} alt="Chicago" style={{width:"100%"}}/>
+    </div> : null
 
-
-    //}
-
-    //console.log(indicatorButtons)
-
-    
 
   return(
     <div className="container">  
   <div style={{backgroundColor:"white"}} id={"myCarousel"+props.carouselId} className="carousel slide shadow" data-ride="carousel">
   
     <ol className="carousel-indicators">
-      <li data-target={"#myCarousel"+props.carouselId} data-slide-to="0" className="active"></li>
-      <li data-target={"#myCarousel"+props.carouselId} data-slide-to="1"></li>
-      <li data-target={"#myCarousel"+props.carouselId} data-slide-to="2"></li>
+      {imageListOne}
+      {imageListTwo}
+      {imageListThree}
     </ol>
 
    
     <div className="carousel-inner">
-      <div className="item active">
-        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-941.jpg" alt="Los Angeles" style={{width:"100%"}}/>
-      </div>
-
-      <div className="item">
-        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-101.jpg" alt="Chicago" style={{width:"100%"}}/>
-      </div>
-    
-      <div className="item">
-        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-1051.jpg" alt="New york" style={{width:"100%"}}/>
-      </div>
+      
+      {imageDisplayOne}
+      {imageDisplayTwo}
+      {imageDisplayThree}
     </div>
 
     <a className="left carousel-control" href={"#myCarousel"+props.carouselId} data-slide="prev">
