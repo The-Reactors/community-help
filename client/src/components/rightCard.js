874eee5b5f4171e717ed7de78cff6145405a5943
shivamdargan <darganshivam2001@gmail.com>
2021-09-14 04:17:03 +0530

    Added Filter Card-Front-ENd

--- client/src/components/rightCard.js
+++ client/src/components/rightCard.js
@@ -9,7 +9,7 @@ import {Button} from 'react-bootstrap'
 
 
 const RightCard = (props) => {
-    const [ value, setValue ] = useState(0);
+    const [ value, setValue ] = useState(10);
 
     
 
@@ -18,14 +18,10 @@ const RightCard = (props) => {
         <div>
         <Card style = {{ width: '18rem' }}>
             <Card.Body>
-                <Card.Title style = {{alignContent:"centre",padding:"2rem"}} ><i className="fa fa-filter"></i> Filters</Card.Title>
+                <Card.Title style = {{textAlign:"center",padding:"2rem"}} ><i className="fa fa-filter"></i> Filters</Card.Title>
             
                      
-                <Button variant="secondary" style = {{marginLeft:"1rem"}}>
-                     
-                     proximity
-                 
-                 </Button>
+                <div style={{fontSize:"15px",padding:"0.5rem",fontWeight:"bold"}}> Filter By Proximity </div>
                  <br />
                  <RangeSlider
                  value={value}
@@ -33,41 +29,35 @@ const RightCard = (props) => {
                  min = "10"
                  max = "50"
                  onChange={changeEvent => setValue(changeEvent.target.value)}/> 
-                                                
-               
-                    <Button variant="secondary" style = {{marginLeft:"1rem",marginBottom:"1rem"}}>
-                     
-                         Upvotes
-                     
-                     </Button>
-                     <Dropdown style = {{padding:"1rem"}}>
-                     <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                     <Dropdown >
+                        <div style={{fontSize:"15px",padding:"0.5rem",fontWeight:"bold"}}> Choose A Category </div>
+                     <Dropdown.Toggle variant="primary" id="dropdown-basic">
                          Categories
                      </Dropdown.Toggle>
                      <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1">land issue</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">water issue</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">public health</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4">sanitation</Dropdown.Item>
+                        <Dropdown.Item href="#/action-1">Land issue</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Water issue</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Public health</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4">Sanitation</Dropdown.Item>
                         <Dropdown.Item href="#/action-5">Pollution</Dropdown.Item>
-                        <Dropdown.Item href="#/action-6">healthcare issue</Dropdown.Item>
+                        <Dropdown.Item href="#/action-6">Healthcare issue</Dropdown.Item>
                         <Dropdown.Item href="#/action-7">Electricity</Dropdown.Item>
                         <Dropdown.Item href="#/action-9">Road Blockage</Dropdown.Item>
-                        <Dropdown.Item href="#/action-10">waste management</Dropdown.Item>
+                        <Dropdown.Item href="#/action-10">Waste management</Dropdown.Item>
                      </Dropdown.Menu>
                      </Dropdown>
-                     
-                     <Dropdown style = {{padding:"1rem"}}>
-                     <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                     <div style={{fontSize:"15px",padding:"0.5rem",fontWeight:"bold"}}> Choose Priority </div>
+                     <Dropdown >
+                     <Dropdown.Toggle variant="primary" id="dropdown-basic">
                          Urgency
                      </Dropdown.Toggle>
                      <Dropdown.Menu>
-                        <Dropdown.Item href="#/action1">emergency</Dropdown.Item>
-                        <Dropdown.Item href="#/action2">urgent</Dropdown.Item>
-                        <Dropdown.Item href="#/action3">not urgent</Dropdown.Item>
-                        <Dropdown.Item href="#/action4">baad mein karwalenge</Dropdown.Item>
+                        <Dropdown.Item href="#/action1">Emergency</Dropdown.Item>
+                        <Dropdown.Item href="#/action2">Urgent</Dropdown.Item>
+                        <Dropdown.Item href="#/action3">Not urgent</Dropdown.Item>
                      </Dropdown.Menu>
                      </Dropdown>
+            <Button style={{marginTop:"10px"}} variant="success">Filter Results</Button>
             </Card.Body>
         </Card>
         </div>
