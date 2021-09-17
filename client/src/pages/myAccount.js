0589ec38b427517a3fa0de5ca714725d360398a5
utkrisht30 <usingh_be20@thapar.edu>
2021-09-17 13:56:54 +0530

    added functionality to the MyAccount Page and revamped the UI

--- client/src/pages/myAccount.js
+++ client/src/pages/myAccount.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react'
 import swal from 'sweetalert';
 import { useEffect } from 'react';
 import URL from '../URL';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import { Card, CardContent, Typography,CardMedia, Input } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 const MyAccount = () =>{
     
@@ -24,6 +30,21 @@ const MyAccount = () =>{
     const [imageState, setimageState] = useState()
     const [updatedName, setUpdatedName] = useState()
     const [updatedPhoneNo, setUpdatedPhoneNo] = useState()
+    const [disabledName,setDisabledName] = useState(true)
+    const [disabledNumber, setDisabledNumber] = useState(true)
+    const [disabledImage,setdisabledImage] = useState(false)
+
+    const handleEditName = () => {
+        setDisabledName(false)
+       
+    }
+    const handleEditNumber  = ( ) => {
+        setDisabledNumber(false)
+        
+    }
+    const handleEditImage = () => {
+      setdisabledImage(true)
+    }
 
     const handleInputName = (event) =>
     {
@@ -207,7 +228,7 @@ const MyAccount = () =>{
       <div id="content" className="p-4 p-md-5 pt-5">
         <form data-aos="fade-up" data-aos-delay="300"  action="" encType = "multipart/form-data">
               
-        <label htmlFor="name">Name</label>
+        {/* <label htmlFor="name">Name</label>
                 <input 
                 type="text" 
                 autoComplete = "off"
@@ -224,16 +245,92 @@ const MyAccount = () =>{
                 onChange = {handleInputPhoneNo}
                 name = "description"
                 />
-                <button onClick = {fileUpdatePhoneNoHandler}>Update</button>
-              <p>
+                <button onClick = {fileUpdatePhoneNoHandler}>Update</button> */}
+              {/* <p>
                 <label htmlFor="uploads">
                 Update Your Profile Picture
                 </label>
                 <input type="file" id="uploads" name="uploads" accept=".jpg, .jpeg, .png, .svg, .gif" onChange = {fileHandler} />
                 <button onClick = {fileSubmitHandler}>Upload</button>
-                </p>
+                </p> */}
                 
            </form> 
+         <Card>
+         <CardContent>
+         <Typography style = {{fontWeight:"bolder"}} color="textDark" gutterBottom variant="h2">
+        My Account
+        </Typography>
+         
+        <TextField
+          disabled = {disabledName}
+          id="outlined-disabled"
+          label="Name"
+          defaultValue={updatedName}
+          autoComplete = "off"
+          value = {updatedName}
+          onChange = {handleInputName}
+          name = "description"
+          helperText = "click on the edit icon to update your name"
+        />
+        <IconButton onClick = {handleEditName}>
+          <EditIcon />
+        </IconButton>
+        <TextField
+          disabled = {disabledNumber}
+          id="outlined-number"
+          label="number"
+          defaultValue={updatedPhoneNo}
+          type="number"
+          autoComplete = "off"
+          value = {updatedPhoneNo} 
+          onChange = {handleInputPhoneNo}
+          name = "description"
+          helperText = "click on the edit icon to update your number"
+        />
+        <IconButton onClick = {handleEditNumber}>
+          <EditIcon />
+        </IconButton>
+        <Typography>
+        <Button hidden = {disabledName} variant = "contained" color = 'secondarary' onClick = {fileUpdateNameHandler} style = {{marginRight:'11rem'}}>Change Name</Button>
+        <Button hidden = {disabledNumber} variant = "contained" color = 'secondarary' onClick = {fileUpdatePhoneNoHandler}>Change Number</Button>
+        </Typography>
+        <br />
+        <br />
+        <hr />
+        <CardMedia
+            alt="profile picture"
+            component="img"
+            title="Profile Picture"
+            height = '450'
+            image=
+"https://cdn.pixabay.com/photo/2016/01/03/00/43/upload-1118929_960_720.png"
+          />
+        
+                <Typography htmlFor="uploads">
+                Update Your Profile Picture by clicking on the edit icon
+                </Typography>
+                <IconButton onClick = {handleEditImage}>
+                <EditIcon />
+                </IconButton>
+                {disabledImage && <div><Typography align = "center"><Input type="file" id="uploads" name="uploads" accept=".jpg, .jpeg, .png, .svg, .gif" onChange = {fileHandler} />
+                <Button variant = "contained" color = "primary" onClick = {fileSubmitHandler}>Upload</Button></Typography></div>}
+                
+                
+                </CardContent>
+          </Card>
+
+           </div>
+           <div>
+           {/* <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" }
+      }}
+      noValidate
+      autoComplete="off"
+    > */}
+        
+    {/* </Box> */}
            </div>
     </div>
 }
