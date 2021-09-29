7c987c5f0a4ac8c64199375514c34241e9c8743a
shivamdargan <darganshivam2001@gmail.com>
2021-09-30 03:05:37 +0530

    Fixed DIRNAME path

--- src/index.js
+++ src/index.js
@@ -65,9 +65,9 @@ app.listen(port, ()=>{
 // * Production setup
 if (process.env.NODE_ENV === "production") {
   console.log("prod");
-  app.use(express.static(path.resolve(__dirname, "client/build")));
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
   app.get("/*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 
   // Handle unhandled promise rejections
